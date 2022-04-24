8fa193c6afe526f234aa427a74015042a9be0d2b
takeYY <t0k6yist@gmail.com>
2022-04-24 22:48:53 +0900

    🔥 javascript

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "../styles/global.css";
-import "highlight.js/styles/tokyo-night-dark.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
